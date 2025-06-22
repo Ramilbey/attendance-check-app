201f5ace3ce5e7ab271286f634615d167b5b7898
Ramilbey <ramilbey505711503@gmail.com>
2025-06-22 16:50:40 +0800

    rename Student.js to Temp.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Student = require('../models/Student');
+const Student = require('../models/Temp');
 
 
 // 📌 Create a student
