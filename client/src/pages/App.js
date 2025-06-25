05a196d7572775714b564105912f514d13720c9c
Ramilbey <ramilbey505711503@gmail.com>
2025-06-25 23:18:33 +0800

    fix importing

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentCheck from "./pages/StudentCheck";
-import StudentDashboard from "./pages/StudentDashboard";
+import StudentCheck from "./StudentCheck";
+import StudentDashboard from "./StudentDashboard";
 
 function App() {
   return (
