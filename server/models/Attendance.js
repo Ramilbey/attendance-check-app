7b5170da4e9130c299964b966ca7e9492c78969b
Ramilbey <ramilbey505711503@gmail.com>
2025-06-25 19:31:17 +0800

    update attendace js to post with stiudent id, not with random id

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -1,25 +1,24 @@
-// models/Attendance.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const attendanceSchema = new mongoose.Schema({
   studentId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Student',
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  status: {
-    type: String,
-    enum: ['Present', 'Absent'],
+    ref: "Student",
     required: true,
   },
   lesson: {
     type: String,
     required: true,
   },
+  status: {
+    type: String,
+    enum: ["Present", "Absent"],
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Attendance', attendanceSchema);
+module.exports = mongoose.model("Attendance", attendanceSchema);
